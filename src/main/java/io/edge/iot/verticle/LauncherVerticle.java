5bf0a442efe59744509896b6bf167bdce6fed379
Sebastien <Sebastien@192.168.1.28>
2019-04-14 10:01:55 +0200

    Use default Configuration Retriver

--- src/main/java/io/edge/iot/verticle/LauncherVerticle.java
+++ src/main/java/io/edge/iot/verticle/LauncherVerticle.java
@@ -1,15 +1,9 @@
 package io.edge.iot.verticle;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.CompositeFuture;
@@ -19,26 +13,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 
 public class LauncherVerticle extends AbstractVerticle {
-
-	private void readConfig(Handler<AsyncResult<JsonObject>> resultHandler) {
-
-		InputStream inputStream = this.getClass().getResourceAsStream("/default-config.json");
-
-		JsonObject defaultConfig = new JsonObject(new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n")));
-
-		ConfigStoreOptions defaultStore = new ConfigStoreOptions().setType("json").setConfig(defaultConfig);
-
-		ConfigStoreOptions envStore = new ConfigStoreOptions().setType("env");
-
-		ConfigStoreOptions sysStore = new ConfigStoreOptions().setType("sys");
-
-		ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(defaultStore).addStore(envStore).addStore(sysStore);
-
-		ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
-
-		retriever.getConfig(resultHandler);
-
-	}
 
 	private void deployVerticles(JsonObject config, Handler<AsyncResult<Void>> handlerResult) {
 
@@ -107,11 +81,11 @@ public class LauncherVerticle extends AbstractVerticle {
 	@Override
 	public void start(Future<Void> startFuture) {
 
-		this.readConfig(configResult -> {
+		ConfigRetriever.create(vertx).getConfig(ar -> {
 
-			if (configResult.succeeded()) {
+			if (ar.succeeded()) {
 
-				this.deployVerticles(configResult.result(), deployResult -> {
+				this.deployVerticles(ar.result(), deployResult -> {
 
 					if (deployResult.succeeded()) {
 						startFuture.complete();
@@ -122,7 +96,7 @@ public class LauncherVerticle extends AbstractVerticle {
 				});
 
 			} else {
-				startFuture.fail(configResult.cause());
+				startFuture.fail(ar.cause());
 
 			}
 
